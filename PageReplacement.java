9de1b4d89d4213fe8478281bd8568ff079bec50c
sivaramalingamk <30752256+sivaramalingamk@users.noreply.github.com>
2017-11-14 10:58:23 +0100

    Create PageReplacement.java

---
+++ PageReplacement.java
@@ -0,0 +1,18 @@
+public class PageReplacement{
+
+  int a[][];
+
+  PageReplacement(int frameNos){
+    a=new int [frameNos][frameNos];  
+  }
+
+  public void LRU(int req[]){
+  
+  }
+
+  public static void main(String ar[]){
+  int n;
+  System.out.println("Enter the Number of Frames")
+  PageReplacement pr=new PageReplacement(n);
+  }
+}
