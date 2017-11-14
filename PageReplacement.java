6fac02363f4aa4c3c9ca43a39490671190a811bb
sivaramalingamk <30752256+sivaramalingamk@users.noreply.github.com>
2017-11-14 11:36:57 +0100

    Update PageReplacement.java

--- PageReplacement.java
+++ PageReplacement.java
@@ -1,25 +1,47 @@
 import java.util.*;
 public class PageReplacement{
 
-  int a[][];
+  
 
   PageReplacement(int frameNos){
-    a=new int [frameNos][frameNos];  
+      
   }
 
-  public void LRU(int req[]){
+  
+    public void secondChance(int req[]){
   
   }
+    public void lru(int req[]){
+  
+  }
+  
+    public void nru(int req[]){
+  
+}
+    public void fifo(int req[]){
+  
+}
+    public void clock(int req[]){
+  
+}
+  
 
   public static void main(String ar[]){
-    int n,s,a[][];
+    int n,s,a[];
     Scanner sc=new Scanner(System.in());
    
     System.out.println("Enter the Number of Frames");
     n=sc.nextInt(); 
     
     System.out.println("Enter the Reference Array size");
-    n=sc.nextInt(); 
+    s=sc.nextInt();
+    a=new int [s];
+    
+    for(int i=0;i<s;i++){
+      System.out.println("Enter "+i+"th referencing page");
+      a[i]=sc.nextInt();
+    }
+    
     
     PageReplacement pr=new PageReplacement(n);
   }
